38d4d913dd6cbd1baec9d2476835612e1528551e
Alex Nagy <therebeloper@gmail.com>
2021-01-27 13:18:35 +0200

    exclude

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HUDKit",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["HUDKit/HUDKit.png"]),
         .testTarget(
             name: "HUDKitTests",
             dependencies: ["HUDKit"]),
